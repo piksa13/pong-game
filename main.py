4d0dbe676c0fd85713bd58d5b36e2598a33de0d4
piksa13 <ania.piktas@gmail.com>
2024-12-23 12:20:24 +0100

    Left paddle

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
 screen.title("Pong game")
+screen.tracer(0) #turn off animation
 
-paddle = Paddle()
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+game_is_on = True
 
 screen.listen()
-screen.onkey(fun=paddle.up, key="Up")
-screen.onkey(fun=paddle.down, key="Down")
+screen.onkey(fun=r_paddle.up, key="Up")
+screen.onkey(fun=r_paddle.down, key="Down")
+screen.onkey(fun=l_paddle.up, key="w")
+screen.onkey(fun=l_paddle.down, key="s")
 
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -4,18 +4,21 @@ DOWN = 270
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self,position):
         super().__init__()
         self.shape("square")
-        self.shapesize(stretch_wid=5, stretch_len=1)
         self.color("white")
         self.penup()
-        self.goto(350, 0)
+        self.speed("fastest")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.position = position
+        self.goto(self.position[0], self.position[1])
+
 
     def up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        new_y = self.position[1] + 20
+        self.goto(self.position[0], new_y)
 
     def down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+        new_y = self.position[1] - 20
+        self.goto(self.position[0], new_y)
