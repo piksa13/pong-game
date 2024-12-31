86517f7f883c5308224d8419c4822ab37d6856d7
piksa13 <ania.piktas@gmail.com>
2024-12-31 16:40:59 +0100

    Adding a scoreboard

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,7 +14,7 @@ screen.tracer(0) #turn off animation
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 game_is_on = True
 
 screen.listen()
@@ -24,17 +25,27 @@ screen.onkey(fun=l_paddle.down, key="s")
 
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
+
     #Collision with the top/ bottom wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
+
     #Collision with paddles
     if ball.distance(r_paddle ) < 50 and ball.xcor() > 320 or ball.distance(l_paddle ) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    #Detect missing paddle
-    if ball.distance(r_paddle ) > 50 and ball.xcor() > 320 or ball.distance(l_paddle ) > 50 and ball.xcor() < -320:
+    #Detect missing right paddle
+    if ball.xcor() > 380:
         ball.reset_position()
+        ball.bounce_x()
+        scoreboard.l_point()
+
+    #Detect missing left paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+        ball.bounce_x()
+        scoreboard.r_point()
 
 screen.exitonclick()
